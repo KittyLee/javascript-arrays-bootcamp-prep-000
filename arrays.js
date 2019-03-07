670a68443a4ebdba4548eb617be2217595f09e38
elegant-architecture-3891 <katherineortiz313@gmail.com>
2019-03-07 17:40:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -17,7 +17,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 
 function addElementToEndOfArray(array, element) {
   array = [...array, element]
-  return [element, array]
+  return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
