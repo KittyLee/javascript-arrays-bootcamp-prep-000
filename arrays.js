c13ac0d4fef514eaa46078a4594d69173a61ff8f
elegant-architecture-3891 <katherineortiz313@gmail.com>
2019-03-07 17:11:34 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,7 @@ function addElementToEndOfArray(array, element) {
 
 function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
-  return [array]
+  return [element, array]
 }
 
 function accessElementInArray(array, index) {
