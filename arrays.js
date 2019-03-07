6dba752041427addede9d130f6975164e1f533d4
elegant-architecture-3891 <katherineortiz313@gmail.com>
2019-03-07 17:53:07 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -35,8 +35,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1)
-  return array 
+  array = array.slice(1)
+  return array
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
