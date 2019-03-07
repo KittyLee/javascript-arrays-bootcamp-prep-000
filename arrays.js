2cd37189782e78ffac4c17609a223d308e516642
elegant-architecture-3891 <katherineortiz313@gmail.com>
2019-03-07 16:36:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,11 +6,21 @@ var chocolateBars = [
 ]
 
 function addElementToBeginningOfArray(array, element) {
-  addElementToBeginningOfArray = ["element", ...array]
-  return element, array
+  addElementToBeginningOfArray.unshift("element");
+  return [element, array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
+}
+
+function addElementToEndOfArray(array, element) {
+  addElementToEndOfArray.push("element");
+  return [element, array]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return [array]
 }
